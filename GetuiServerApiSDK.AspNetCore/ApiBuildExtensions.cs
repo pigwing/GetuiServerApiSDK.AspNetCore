c783589d82b9a9839cdff5835a57ea44a3614912
pigwing <xpigwing@gmail.com>
2020-05-04 18:53:12 +0800

    增加预先获取auto token,应对启动的时候并发请求.造成部分token返回null

---
+++ GetuiServerApiSDK.AspNetCore/ApiBuildExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using GetuiServerApiSDK.AspNetCore.Clients;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GetuiServerApiSDK.AspNetCore
+{
+    public static class ApiBuildExtensions
+    {
+        public static IApplicationBuilder UseQuartz(this IApplicationBuilder applicationBuilder,
+            IServiceProvider serviceProvider)
+        {
+            AuthTokenService authTokenService = serviceProvider.GetService<AuthTokenService>();
+            GetuiConfiguration getuiConfiguration = serviceProvider.GetService<GetuiConfiguration>();
+            if(getuiConfiguration == null)
+                throw new ArgumentNullException(nameof(GetuiConfiguration));
+            //提前获取auto token防止启动时候的并发发送
+            Task.Run(async () =>
+            {
+                await authTokenService.GetAuthToken();
+            });
+            
+
+            return applicationBuilder;
+        }
+    }
+}
